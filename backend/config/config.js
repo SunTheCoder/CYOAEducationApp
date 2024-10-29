1d118a498348cd98a0edf48c90301ea3fb4cedce
SunTheCoder <sunenglishjr@gmail.com>
2024-10-29 18:37:49 -0400

    deploy debug

--- backend/config/config.js
+++ backend/config/config.js
@@ -16,10 +16,7 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_URL,
+    use_env_variable: 'DATABASE_URL', // This will use the full DATABASE_URL connection string
     dialect: 'postgres'
   }
 };
