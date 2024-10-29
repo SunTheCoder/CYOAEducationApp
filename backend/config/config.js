181c7dab4c19f24148b305bfa71af0a1ffbc1fd8
SunTheCoder <sunenglishjr@gmail.com>
2024-10-29 18:40:40 -0400

    deploy debug

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,10 +2,8 @@ require('dotenv').config(); // Load environment variables
 
 module.exports = {
   development: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: '127.0.0.1',
+    use_env_variable: 'DATABASE_URL',
+    
     dialect: 'postgres'
   },
   test: {
