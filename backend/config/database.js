3ede9170d49e2c8270fef58a202742e42801143b
SunTheCoder <sunenglishjr@gmail.com>
2024-10-29 20:09:23 -0400

    render env url

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
-  host: process.env.DATABASE_HOST || 'localhost',
-  dialect: 'postgres',
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || `postgres://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST || 'localhost'}/${process.env.DATABASE_NAME}`,
+  {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false, // Set to console.log to debug SQL queries
+    dialectOptions: {
+      ssl: process.env.NODE_ENV === 'production' ? { require: true, rejectUnauthorized: false } : false,
+    },
+  }
+);
 
 module.exports = sequelize;
